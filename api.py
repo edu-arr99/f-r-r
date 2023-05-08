9af7229802080b6aea3884f75c743ee819d13c4d
edu-arr99 <eduardo.arrospide@utec.edu.pe>
2023-05-07 23:40:12 -0500

    Final

--- api.py
+++ api.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask import request, jsonify
 import logging
+from flask_cors import CORS
+import bcrypt
 # Here is a missing import ? Are we missing some hashing library ???
 
 app = Flask(__name__)
+CORS(app)
 
-secretPass = "abcd";
-API_KEY_VAL = "12345";
+secretPass = "abcd"
+API_KEY_VAL = "12345"
 
 @app.route('/external-api', methods=['POST'])
 def happy():
@@ -15,7 +18,7 @@ def happy():
         return jsonify(error="request body cannot be empty"), 400
     #print(data)
     api_key_val = data["api_key"]
-    if api_key_val != API_KEY_VAL:
+    if not bcrypt.checkpw(API_KEY_VAL.encode('utf-8'), api_key_val.encode('utf-8')):
         return jsonify(result="You are not welcome Here !!")
     return jsonify(result="Im your father Luke !")
 
--- front/basicui/src/App.js
+++ front/basicui/src/App.js
@@ -1,21 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 import bcrypt from 'bcryptjs'
 
 function App() {
 
-  const [data, setData] = useState({data: []});
+  const [data, setData] = useState({ data: [] });
   const [err, setErr] = useState('');
   const API_KEY = "12345";
   // Hashing
   const password = "abcd"
   const salt = bcrypt.genSaltSync(10) // BCRYPT is the algorithm <====
-  
+
 
   const handleClick = async () => {
     try {
-      const encrypted_api_key = bcrypt.hashSync(API_KEY);
+      const encrypted_api_key = bcrypt.hashSync(API_KEY, salt);
       console.log(encrypted_api_key)
       const response = await fetch('http://localhost:5002/external-api', {
         method: 'POST',
@@ -43,13 +43,13 @@ function App() {
   return (
     <div className="App">
       <div>
-        Call API 
-        <button type="button" onClick={handleClick}>
-        Click Me
-      </button>
-      <div>
-      {data.result}
-      </div>
+        Call API
+        <button class="Button" type="button" onClick={handleClick}>
+          Click Me
+        </button>
+        <div>
+          {data.result}
+        </div>
       </div>
     </div>
   );
